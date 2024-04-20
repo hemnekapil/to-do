0f6c67ac51b935617d71a817683d5ccf976005cc
kapil hemne <hemnekapil24@gmail.com>
2024-04-20 16:27:06 +0530

    Created todo file

---
+++ frontend/src/components/Createtodo.jsx
@@ -0,0 +1,13 @@
+const Createtodo = () => {
+  return (
+    <div>
+      <input type="text" placeholder="title" />
+      <br />
+      <input type="text" placeholder="description" />
+      <br />
+      <button>Add a todo</button>
+    </div>
+  );
+};
+
+export default Createtodo;
